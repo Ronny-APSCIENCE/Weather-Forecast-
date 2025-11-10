18e9ada95741fa31a526c1bd8084c265143407d7
Ronny-APSCIENCE <ronnyillicachi@outlook.com>
2025-11-10 14:20:35 +0000

    Weather project

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+temperature = 20
+print("Will it rain tomorrow?")
 
+if temperature < 20:
+    print("No, Wear jeans and a T-shirt")
+
+elif temperature < 10:
+    print("No,Will it rain:no, wear jeans and a T-Shirt")
+
+elif temperature < 5:
+    print("Mo, Will it rain: NO, Wears jeans, T-Shirt and a sweater.")
+
+else:
+    print("YES! Please wear a warm jacket, jeans and a T-shirt with gloves")
